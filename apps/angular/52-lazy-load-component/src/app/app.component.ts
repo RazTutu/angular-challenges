89c2b5bbd8608e8b6ec7961e2fd220ab28e3cae6
RazTutu <raztutu123@gmail.com>
2025-01-04 20:29:39 +0200

    feat: solve problem

--- apps/angular/52-lazy-load-component/src/app/app.component.ts
+++ apps/angular/52-lazy-load-component/src/app/app.component.ts
@@ -1,23 +1,27 @@
-import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { PlaceholderComponent } from './placeholder.component';
+import { TopComponent } from './top.component';
 
 @Component({
   selector: 'app-root',
   template: `
     <div class="h-screen bg-gray-500">
-      @if (topLoaded()) {
+      @defer (on interaction(trigger)) {
         <app-top />
-      } @else {
-        <app-placeholder />
-        <button
-          class="rounded-sm border border-blue-500 bg-blue-300 p-2"
-          (click)="topLoaded.set(true)">
-          Load Top
-        </button>
+      } @placeholder {
+        <div>
+          <app-placeholder />
+          <button
+            #trigger
+            class="rounded-sm border border-blue-500 bg-blue-300 p-2">
+            Load Top
+          </button>
+        </div>
       }
     </div>
   `,
-  standalone: false,
+  standalone: true,
+  imports: [CommonModule, PlaceholderComponent, TopComponent],
 })
-export class AppComponent {
-  topLoaded = signal(false);
-}
+export class AppComponent {}
